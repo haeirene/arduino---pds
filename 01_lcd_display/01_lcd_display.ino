c9d1b5fcd52a2f31e8fac3df95da577c4e493bfa
haeirene <haegeman.irene@hotmail.com>
2021-12-13 16:58:59 +0100

    [lcd] Add code for lcd display

---
+++ 01_lcd_display/01_lcd_display.ino
@@ -0,0 +1,21 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+// communication 0x27, 16 characters on 2 lines
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+  lcd.begin();
+  // Turn on the blacklight and print a message.
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Neerslag in de ");
+  lcd.setCursor(0, 1);
+  lcd.print("herfst van 2021");
+
+  delay(1000);
+}
